badf9d5933be4a1f39508869175394c66f98c399
Christophe GACHET <maildecri@gmail.com>
2017-11-28 15:35:22 +0100

    Profile view partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'profile/show'
+  get 'profile/edit'
+  post 'profile/update'
+
   devise_for :users,
   controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  
+
   root to: 'pages#home'
-    
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128100040) do
+ActiveRecord::Schema.define(version: 20171128142502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20171128100040) do
     t.float    "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
     t.index ["user_id"], name: "index_user_places_on_user_id", using: :btree
   end
 
@@ -83,6 +84,7 @@ ActiveRecord::Schema.define(version: 20171128100040) do
     t.string   "uid"
     t.string   "token"
     t.datetime "token_expiry"
+    t.string   "description"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
