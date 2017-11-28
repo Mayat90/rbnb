e94520fab8edcce3ce4a2ebf3647ebd1e6b64a63
Christophe GACHET <maildecri@gmail.com>
2017-11-28 15:52:12 +0100

    test2 resolv

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'profile/show'
+
 
   devise_for :users,
   controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root to: 'pages#home'
 
+  resources :user_places do
+    resources :availabilities, only: [ :index, :new, :create, :destroy ]
+  end
+  resources :availabilities, only: [:edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
