bbad0c5450395f729fb08868755650a307d70ead
Christophe GACHET <maildecri@gmail.com>
2017-11-28 12:54:54 +0100

    Google auth done

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -7,6 +7,8 @@ Devise.setup do |config|
     image_size: 'square',  # 50x50, guaranteed ratio
     secure_image_url: true
 
+  config.omniauth :google_oauth2, ENV["GOOGLE_ID"], ENV["GOOGLE_SECRET"], {}
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
