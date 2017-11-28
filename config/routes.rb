0314c13c14b1d67203771214c06b5a6bdd32a3d5
Mattia Lamouroux <mattia.lamouroux@outlook.fr>
2017-11-28 14:24:53 +0100

    Routeswrappee

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+
   devise_for :users,
   controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  
+
   root to: 'pages#home'
-    
+
+  resources :user_places do
+    resources :availabilities, only: [ :index, :new, :create ]
+  end
+  resources :availabilitis, only: [:edit, :update, :destroy ]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
