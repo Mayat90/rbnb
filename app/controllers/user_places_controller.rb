06d94ac171eac40bf1914a3b2b1afd61e8d7b4a2
Tiphanie Tourniaire <tiphanietourniaire@hotmail.com>
2017-11-28 15:18:58 +0100

    Controller userplaces

--- app/controllers/user_places_controller.rb
+++ app/controllers/user_places_controller.rb
@@ -1,22 +1,45 @@
 class UserPlacesController < ApplicationController
+
+  before_action :set_user_place, only: [:show, :edit, :update, :destroy]
   def index
+    @user_places = UserPlaces.all
   end
 
   def show
   end
 
   def new
+    @user_place = UserPlace.new
   end
 
   def create
+    @user_place = UserPlace.new(user_place_params)
+    if @user_place.save
+      redirect_to user_place_path(@user_place)
+    else
+      render :new
   end
 
   def edit
   end
 
   def update
+    @user_place.update_attributes(user_place_params)
+    redirect_to user_place_path(@user_place)
   end
 
   def destroy
+    @user.place.destroy
+    redirect_to user_places_path
+  end
+
+  private
+
+  def set_user_place
+    @user_place = UserPlace.find(params[:id])
+  end
+
+  def user_place_params
+    params.require(:user_place).permit(:address, :capacity, :price)
   end
 end
