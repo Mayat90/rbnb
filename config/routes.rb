eedb02d22b0d345c4f9789a904bf400c5c87c046
Christophe GACHET <maildecri@gmail.com>
2017-11-29 16:53:05 +0100

    Prepare for first demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'profile', to: 'profile#show'
+  get 'profile/edit', to: 'profile#edit'
+  post 'profile/edit', to: 'profile#update'
   # get 'user_places/find', to: 'user_places#find'
 
   devise_for :users,
@@ -14,6 +16,6 @@ Rails.application.routes.draw do
   resources :availabilities, only: [:edit, :update]
 
   resources :reservations, only: [:show, :index, :edit, :update, :destroy]
-
+  mount Attachinary::Engine => "/attachinary"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
