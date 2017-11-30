a6d97e195868b788e1b07cb048792be156b06eb2
Christophe GACHET <maildecri@gmail.com>
2017-11-30 14:12:11 +0100

    Refacto avant reoga

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
   def avatar_img(myclass)
     if current_user.cphoto
         cl_image_tag current_user.cphoto.path, width: 150, height: 150, crop: :fill, class: "avatarimg #{myclass}"
-    elsif current_user.photo != ""
+    elsif current_user.photo != "" && current_user.photo != nil
        image_tag(current_user.photo, class: "avatarimg #{myclass}")
     else
        image_tag("avatar.png", class: "avatarimg #{myclass}")
