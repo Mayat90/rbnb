af87970abe4a9e3a20ddfd3361682df61336645b
Tiphanie Tourniaire <tiphanietourniaire@hotmail.com>
2017-12-01 14:12:09 +0100

    reservation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130135230) do
+ActiveRecord::Schema.define(version: 20171130165316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 20171130135230) do
     t.boolean  "garde_a_domicile"
     t.integer  "capacity"
     t.string   "address"
+    t.float    "price"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
